345a0d3e9c333bd353bbdab2258cd618c932c8ae
Harshal <harshal.madgulkar@sdaemon.co.in>
2025-01-17 17:00:37 +0530

    removed unused link endpoint

--- site/admin/src/bLove/cComponent/aGlobalComponent/outlet/bProtectedComponent/outlet/bAuthorizationComponent/outlet/bSidebarComponent/children/cInvenTechComponent/aOverallComponent/kInventoryGeneralComponent/dStockTransferComponent/index.tsx
+++ site/admin/src/bLove/cComponent/aGlobalComponent/outlet/bProtectedComponent/outlet/bAuthorizationComponent/outlet/bSidebarComponent/children/cInvenTechComponent/aOverallComponent/kInventoryGeneralComponent/dStockTransferComponent/index.tsx
@@ -1,7 +1,6 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import { Button } from "@/aConnection/bShadcnConnection/components/ui/button";
-import endpointRoute from "@/bLove/gRoute/aEndpointRoute";
 import { PlusIcon } from "lucide-react";
 
 const StockTransferComponent = () => {
